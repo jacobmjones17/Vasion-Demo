69f918921c78e2e4758710262fbedc6768f906bc
Jacob Jones <jacobmjones17@gmail.com>
2022-08-31 18:08:49 -0400

    I added a navigation test inside of the firstLogin test.

--- Login-Tests/firstLogin.js
+++ Login-Tests/firstLogin.js
@@ -6,26 +6,45 @@ async function firstLogin() {
     const userName = driver.findElement(By.id("user-name"));
     const password = driver.findElement(By.id("password"));
 
+    
     // open Saucedemo.com
     await driver.get("https://www.saucedemo.com/");
     
+    
+    
+    // Input Username & Password
     userName.clear();
     await userName.sendKeys("standard_user")
-    
     await password.sendKeys("secret_sauce", Key.RETURN)
 
-    // Input Username
     
+    // get current URL
     const promise = driver.getCurrentUrl().then(function(url){
         console.log("\x1B[32mCongratulations, you officially logged in!")
         return url
     })
+    const result = await promise
+    
+    
+    // Actual vs. Expectation URL for Login
+    assert.strictEqual(result, "https://www.saucedemo.com/inventory.html")
+
+    // Click on first item
+    await driver.findElement(By.id("item_4_img_link")).click();
+
+    //Get new current URL
+    const currentUrl = driver.getCurrentUrl().then(function(url) {
+            console.log("\x1B[32mThis is the item you clicked on!")
+            return url
+        })
  
-     const result = await promise
+    const pageResult = await currentUrl
  
-     assert.strictEqual(result, "https://www.saucedemo.com/inventory.html")
+
+    //Actual vs. Expectation URL for item
+    assert.strictEqual(pageResult, "https://www.saucedemo.com/inventory-item.html?id=4")
  
-     await driver.quit()
+    // await driver.quit()
 }
 
-firstLogin();
+firstLogin()
