3368fa85622b448f3adec39424e09ecfee01d2f0
Jacob Jones <jacobmjones17@gmail.com>
2022-08-31 21:19:03 -0400

    I added a new test for clicking on an item and it taking you to the wrong one. I also renamed my file.

--- Tests/incorrectPage.js
+++ Tests/incorrectPage.js
@@ -1,7 +1,7 @@
 const {Builder, By, Key} = require ("selenium-webdriver");
 const assert = require ("assert");
 
-async function thirdLogin() {
+async function incorrectPage() {
     const driver = await new Builder().forBrowser("chrome").build()
     const userName = driver.findElement(By.id("user-name"));
     const password = driver.findElement(By.id("password"));
@@ -15,6 +15,19 @@ async function thirdLogin() {
     
     await password.sendKeys("secret_sauce", Key.RETURN)
 
-    
+    await driver.findElement(By.id("item_4_img_link")).click()
+
+    const currentUrl = driver.getCurrentUrl().then(function (url) {
+        console.log("\x1B[31mWent to the wrong item!")
+        return url
+    })
+
+    const pageResult = await currentUrl
+
+    assert.strictEqual(pageResult, "https://www.saucedemo.com/inventory-item.html?id=4")
+
+    driver.quit();
 
 }
+
+inccorectPage();
