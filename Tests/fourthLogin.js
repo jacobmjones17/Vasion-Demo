4b210e29cab5acd0dc2e0f3a9e4d0a21a6e1db74
Jacob Jones <jacobmjones17@gmail.com>
2022-09-06 00:26:11 -0400

    saved a file

--- Tests/fourthLogin.js
+++ Tests/fourthLogin.js
@@ -10,8 +10,7 @@ async function fourthLogin() {
     const userName = driver.findElement(By.id("user-name"));
     const password = driver.findElement(By.id("password"));
 
-    await driver.manage().window().maximize()
-
+    driver.manage().window().maximize()
 
     await driver.get("https://www.saucedemo.com/");
     
